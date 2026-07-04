7fb5de646522a817a54617aedf8417395be233a1
Dan Rollans <dan@spyninjashq.com>
2026-07-04 05:48:05 -0700

    FIrst push

---
+++ js/danJS.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll('.wishHeart');
+
+elements.forEach(element => {
+  element.addEventListener('click', (event) => {
+    element.classList.toggle('.wishHeart-active'); 
+    console.log('Div clicked!', event.target);
+  });
+});
+
+// Select the elements
+const toggleButton = document.getElementById('product-wishlist');
+const svgPath = document.getElementById('myPath');
+
+// Toggle the class on click
+toggleButton.addEventListener('click', () => {
+  svgPath.classList.toggle('active');
+});
