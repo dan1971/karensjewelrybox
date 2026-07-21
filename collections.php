c6412e58114390122cae37bfe7a015ac0059bd67
Dan Rollans <dan@spyninjashq.com>
2026-07-21 09:33:59 -0700

    pathid

--- collections.php
+++ collections.php
@@ -18,7 +18,7 @@
                             <div id="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
