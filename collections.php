b1551ce1026bb4aa8faa4c2cb217a482481717db
Dan Rollans <dan@spyninjashq.com>
2026-07-21 10:39:57 -0700

    coll

--- collections.php
+++ collections.php
@@ -11,55 +11,14 @@
                     <h3>Wind Chimes</h3>
                 </div>
                 <div class="product-grid">
-                      <!-- Product 1 -->
-<div class="product-card">
-      <div class="product-image" style="background-image: url('images/chime-001.webp')">
-        <div class="product-label">Bestseller</div>
-        <div id="product-wishlist">
-          <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path id="myPath" class="wishheart" d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" stroke-linecap="round" stroke-linejoin="round"></path>
-          </svg>
-        </div>
-      </div>
-      <div class="product-info">
-        <div class="product-category">Wind Chimes</div>
-        <div class="product-name">Golden Windstone</div>
-        <div class="rating">
-          <div class="stars">
-            <div class="star">★</div>
-            <div class="star">★</div>
-            <div class="star">★</div>
-            <div class="star">★</div>
-            <div class="star">★</div>
-          </div>
-          <div class="review-count">57 reviews</div>
-        </div>
-        <div class="price-row">
-          <div>
-            <span class="price">$155</span>
-            <span class="original-price">$119</span>
-          </div>
-          <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-          </button>
-          <div class="cart-tooltip">Add to cart</div>
-        </div>
-      </div>
-      <div class="product-quick-view">Add to cart</div>
-    </div>
-
-
-
-                    <!-- Product 2 -->
-                                        <div class="product-card">
+                    <!-- Product 1 -->
+                    <div class="product-card">
                         <div class="product-image" style="background-image: url('images/chime-001.webp')">
                             <div class="product-label">Bestseller</div>
                             <div id="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -97,14 +56,60 @@
                     </div>
 
 
+                    <!-- Product 2 -->
+                    <div class="product-card">
+                        <div class="product-image" style="background-image: url('images/chime-002.webp')">
+                            <div class="product-label">Bestseller</div>
+                            <div class="product-wishlist">
+                                <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="myPath" class="wishheart"
+                                        d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+                                        stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="product-info">
+                            <div class="product-category">Wind Chimes</div>
+                            <div class="product-name">Bronze Hearthstone</div>
+                            <div class="rating">
+                                <div class="stars">
+                                    <div class="star">★</div>
+                                    <div class="star">★</div>
+                                    <div class="star">★</div>
+                                    <div class="star">★</div>
+                                    <div class="star">★</div>
+                                </div>
+                                <div class="review-count">57 reviews</div>
+                            </div>
+                            <div class="price-row">
+                                <div>
+                                    <span class="price">$155</span>
+                                    <span class="original-price">$119</span>
+                                </div>
+                                <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
+                                    <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path id="myPath" class="wishheart"
+                                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                    </svg>
+                                </button>
+                                <div class="cart-tooltip">Add to cart</div>
+                            </div>
+                        </div>
+                        <div class="product-quick-view">Add to cart</div>
+                    </div>
+
+
                     <!-- Product 3 -->
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/chime-003.webp')">
                             <div class="product-label">Bestseller</div>
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -131,7 +136,7 @@
                                 <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -145,7 +150,7 @@
                     <!-- Product 4 -->
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/chime-004.webp')">
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -175,7 +180,7 @@
                                 <button class="add-to-cart" data-product="Wooden Cutting Board" data-price="49">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -192,7 +197,7 @@
                             <div class="product-label">Sale</div>
 
 
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="wishlist-icon" width="16" height="16" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -224,7 +229,7 @@
                                 <button class="add-to-cart" data-product="Windchime-001" data-price="150">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -240,10 +245,10 @@
                             <div class="product-label">Sale</div>
 
 
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path  class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -271,7 +276,7 @@
                                 <button class="add-to-cart" data-product="Windchime-001" data-price="150">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -287,7 +292,7 @@
                             <div class="product-label">Sale</div>
 
 
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="wishlist-icon" width="16" height="16" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -319,7 +324,7 @@
                                 <button class="add-to-cart" data-product="Windchime-001" data-price="150">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -335,10 +340,10 @@
                             <div class="product-label">Sale</div>
 
 
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -431,10 +436,10 @@
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/painting-002.webp')">
                             <div class="product-label">Bestseller</div>
-                            <div id="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path  class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -461,7 +466,7 @@
                                 <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
@@ -477,10 +482,10 @@
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/painting-003.webp')">
                             <div class="product-label">Bestseller</div>
-                            <div id="product-wishlist" class="product-wishlist">
+                            <div class="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path  class="wishheart"
+                                    <path id="myPath" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
@@ -507,7 +512,7 @@
                                 <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
                                     <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path  class="wishheart"
+                                        <path id="myPath" class="wishheart"
                                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                             stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
