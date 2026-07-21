7017df059a71d46d11564e2e0768c9473b0d6ac4
Dan Rollans <dan@spyninjashq.com>
2026-07-21 09:23:31 -0700

    product cards

--- collections.php
+++ collections.php
@@ -57,8 +57,8 @@
 
 
                     <!-- Product 2 -->
-                    <div class="product-card">
-                        <div class="product-image" style="background-image: url('images/chime-002.webp')">
+                                        <div class="product-card">
+                        <div class="product-image" style="background-image: url('images/chime-001.webp')">
                             <div class="product-label">Bestseller</div>
                             <div id="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
@@ -71,7 +71,7 @@
                         </div>
                         <div class="product-info">
                             <div class="product-category">Wind Chimes</div>
-                            <div class="product-name">Bronze Hearthstone</div>
+                            <div class="product-name">Golden Windstone</div>
                             <div class="rating">
                                 <div class="stars">
                                     <div class="star">★</div>
@@ -88,11 +88,10 @@
                                     <span class="original-price">$119</span>
                                 </div>
                                 <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
-                                    <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
+                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path id="myPath" class="wishheart"
-                                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                        <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
                                 </button>
                                 <div class="cart-tooltip">Add to cart</div>
