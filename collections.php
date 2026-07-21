e69094382e61b9815c42bec0f7fa9ff0a32ea98f
Dan Rollans <dan@spyninjashq.com>
2026-07-21 15:38:37 -0700

    added product 1

--- collections.php
+++ collections.php
@@ -17,53 +17,43 @@
                    
 
                     <!-- Product 5 -->
-                    <div class="product-card">
-                        <div class="product-image" style="background-image: url('images/chime-005.webp')">
-                            <div class="product-label">Sale</div>
-
-
-                            <div class="product-wishlist">
-                                <svg class="wishlist-icon" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round"></path>
-                                </svg>
-                            </div>
-
-                        </div>
-                        <div class="product-info">
-                            <div class="product-category">Wind Chimes</div>
-                            <div class="product-name">Silver Skystone</div>
-                            <div class="rating">
-                                <div class="stars">
-                                    <div class="star">★</div>
-                                    <div class="star">★</div>
-                                    <div class="star">★</div>
-                                    <div class="star">★</div>
-                                    <div class="star">☆</div>
-                                </div>
-                                <div class="review-count">21 reviews</div>
-                            </div>
-                            <div class="price-row">
-                                <div>
-                                    <span class="price">$249</span>
-                                    <span class="original-price">$329</span>
-                                </div>
-                                <button class="add-to-cart" data-product="Windchime-001" data-price="150">
-                                    <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path id="myPath" class="wishheart"
-                                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                                            stroke-linecap="round" stroke-linejoin="round"></path>
-                                    </svg>
-                                </button>
-                                <div class="cart-tooltip">Add to cart</div>
-                            </div>
-                        </div>
-                        <div class="product-quick-view">Add to cart</div>
-                    </div>
+               <div class="product-card">
+      <div class="product-image" style="background-image: url('images/chime-001.webp')">
+        <div class="product-label">Bestseller</div>
+        <div id="product-wishlist">
+          <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path id="myPath" class="wishheart" d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </div>
+      </div>
+      <div class="product-info">
+        <div class="product-category">Wind Chimes</div>
+        <div class="product-name">Golden Windstone</div>
+        <div class="rating">
+          <div class="stars">
+            <div class="star">★</div>
+            <div class="star">★</div>
+            <div class="star">★</div>
+            <div class="star">★</div>
+            <div class="star">★</div>
+          </div>
+          <div class="review-count">57 reviews</div>
+        </div>
+        <div class="price-row">
+          <div>
+            <span class="price">$155</span>
+            <span class="original-price">$119</span>
+          </div>
+          <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </button>
+          <div class="cart-tooltip">Add to cart</div>
+        </div>
+      </div>
+      <div class="product-quick-view">Add to cart</div>
+    </div>
                     <!-- Product 5 -->
       
                     <!-- Product 6 -->
