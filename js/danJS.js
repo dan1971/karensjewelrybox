098632a2c18ac8aa3f6881d52dbae8aa8f84ec35
Dan Rollans <dan@spyninjashq.com>
2026-07-21 15:47:15 -0700

    css

--- js/danJS.js
+++ js/danJS.js
@@ -1,5 +1,5 @@
 // Select the elements
-const toggleButton = document.getElementById('product-wishlist');
+const toggleButton = document.getElementByClassName('product-wishlist');
 const svgPath = document.getElementByClassName('wishHeart');
 
 // Toggle the class on click
