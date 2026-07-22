d0d351ab8e94abfe791b296472e3aed1c6e4ca86
Dan Rollans <dan@spyninjashq.com>
2026-07-22 05:57:03 -0700

    css

--- collections.php
+++ collections.php
@@ -15,7 +15,7 @@
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/chime-001.webp')">
                             <div class="product-label">Bestseller</div>
-                            <div class="heart" id="pproduct-wishlist">
+                            <div id="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path id="myPath" onclick="this.classList.toggle('active')" class="wishheart"
