a02c97983cb47666bc8cddffce445a0c2b8e506f
Dan Rollans <dan@spyninjashq.com>
2026-07-22 05:50:07 -0700

    click

--- collections.php
+++ collections.php
@@ -15,10 +15,10 @@
                     <div class="product-card">
                         <div class="product-image" style="background-image: url('images/chime-001.webp')">
                             <div class="product-label">Bestseller</div>
-                            <div id="product-wishlist">
+                            <div class="heart" id="product-wishlist">
                                 <svg class="windchime_001" width="16" height="16" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path id="myPath" class="wishheart"
+                                    <path id="myPath" onclick="this.classList.toggle('active')" class="wishheart"
                                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                                         stroke-linecap="round" stroke-linejoin="round"></path>
                                 </svg>
