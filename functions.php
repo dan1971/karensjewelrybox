d5d6eb784f67a6828316adea58e220a6c2585d1b
Dan Rollans <danrollans@yahoo.com>
2026-07-26 10:11:28 -0700

    badges

--- functions.php
+++ functions.php
@@ -63,9 +63,14 @@
     <?php } ?>
     <?php function footer() { ?>
     <section class="footer-badges" aria-label="Store benefits">
-        <div class="badge truck">
+        <div class="badge">
+        <div>
+        
+        </div>
+         <div>
             <strong>Free Shipping</strong>
             <span>On orders over $75</span>
+        </div>
         </div>
         <div class="badge lock">
             <strong>Secure Checkout</strong>
