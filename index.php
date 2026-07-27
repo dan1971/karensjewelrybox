3ec01ef9a4a03dffc99a2f534dbfa6a267002936
Dan Rollans <danrollans@yahoo.com>
2026-07-27 01:20:48 -0700

    trimmed

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
                 <h1>Handmade with Love</h1>
                 <p>Artisanal jewelry crafted with care, inspired by nature, and made to be uniquely yours.</p>
                 <div class="hero-actions">
-                    <a class="button button-dark" data-text="Shop the Collection" href="collections.php">
-                      Shop the Collection
-                    </a>
+                    <a class="button button-dark" data-text="Shop the Collection" href="collections.php">Shop the Collection</a>
                     <a class="button button-light" href="#">Explore New Arrivals</a>
                 </div>
             </div>
