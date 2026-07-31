a7ce8e16ae79b49666820081eaebb0323c57ceb0
Dan Rollans <dan@spyninjashq.com>
2026-07-31 08:07:20 -0700

    Fix selector for product grid in JavaScript to ensure correct element targeting

--- js/danJS.js
+++ js/danJS.js
@@ -1,7 +1,7 @@
 
 
 const words = ["Sugar", "Buddy", "Chimera", "Shrimps", "Lollypop", "Bubbles", "Twinkle", "Sparkle", "Doodle", "Noodle", "Wiggle", "Jellybean"];
-const el = document.querySelector(".product-card");
+const el = document.querySelector(".product-grid");
 const randomWord = words[Math.floor(Math.random() * words.length)];
 for (const child of el.children) {
   console.log(child); // Access each child element here
