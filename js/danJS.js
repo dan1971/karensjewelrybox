7197f53c0702bec8d7c36653e183a0623b487d97
Dan Rollans <dan@spyninjashq.com>
2026-08-02 16:05:18 -0700

    Fix console log statement for cart badge selector to use getAttribute method

--- js/danJS.js
+++ js/danJS.js
@@ -30,7 +30,7 @@ const selectors = {
     addToCartButtons: '[data-product][data-price]',
     productCards: '.product-card'
 };
-console.log('Selectors:', selectors.cartbadge.attr('id'));
+console.log('Selectors:', selectors.cartbadge.getAttribute('id'));
 
 window.addEventListener('DOMContentLoaded', () => {
     initializeCartState();
