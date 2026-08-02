b1db6605205b94ca9a82ee58ac8381856204a846
Dan Rollans <dan@spyninjashq.com>
2026-08-02 14:41:05 -0700

    Remove add-to-cart button and replace with tooltip for improved UI clarity

--- collections.php
+++ collections.php
@@ -89,13 +89,7 @@
                                     <span class="price">$155</span>
                                     <span class="original-price">$119</span>
                                 </div>
-                                <button class="add-to-cart" data-product="Minimalist Vase Set" data-price="89">
-                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </button>
+                     
                                 <div class="cart-tooltip">Add to cart</div>
                             </div>
                         </div>
