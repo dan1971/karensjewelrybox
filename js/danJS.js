93725378fb7af1446103adfa04bc962d7d8253c7
Dan Rollans <danrollans@yahoo.com>
2026-08-06 22:31:46 -0700

    Fix cart item index reference in setupGlobalInteractions function

--- js/danJS.js
+++ js/danJS.js
@@ -262,7 +262,7 @@ function setupGlobalInteractions() {
      console.log("select function "+ indx);
             const quantitySelect = document.getElementById(`quantity${indx}`);
             quantitySelect.addEventListener('change', () => {
-            const cartItemIndex = cartItemIndexSS.price;
+            const cartItemIndex = cartItemIndex.price;
             console.log("STEP 1= "+ cartItemIndex);
 
             // document.querySelector(`.cart-item-price[data-item-price-index="${cartItemIndex}"]`).value;
