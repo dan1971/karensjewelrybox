ed2b14a6f0bce8c3333df18c4c2188445e3e9432
Dan Rollans <danrollans@yahoo.com>
2026-08-07 01:58:58 -0700

    Fix checkout subtotal assignment to convert dataset to string

--- js/danJS.js
+++ js/danJS.js
@@ -319,7 +319,7 @@ function setupPurchasePage() {
         const taxAmount = checkOutSubtotal * taxRate;
         const cartTotal = checkOutSubtotal + shippingCost + taxAmount;
     
-        checkOutSubtotal.innerHTML =cartItemTotal.dataset;
+        checkOutSubtotal.innerHTML =cartItemTotal.dataset.toString();
          checkOutShipping.innerHTML =shippingCost.toString();
          checkOutTax.innerHTML =taxAmount.toString();
          checkOutTotal.innerHTML =cartTotal.toString();
