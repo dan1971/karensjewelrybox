dbf8a0ebe871f36cfc4b963ee8257aa9c9615d8f
Dan Rollans <danrollans@yahoo.com>
2026-08-06 22:45:38 -0700

    Fix quantity change event listener to correctly update cart item total

--- js/danJS.js
+++ js/danJS.js
@@ -262,14 +262,15 @@ function setupGlobalInteractions() {
      console.log("select function "+ indx);
             const quantitySelect = document.getElementById(`quantity${indx}`);
             quantitySelect.addEventListener('change', () => {
-            
+            const quantitySelected = quantitySelect.value;
             console.log("STEP 1= "+ quantitySelect.value);
 
             // document.querySelector(`.cart-item-price[data-item-price-index="${cartItemIndex}"]`).value;
             // const userCard = document.querySelector(`.profile-card[data-item-price-index="${userId}"]`);
-const cartItemTotal=document.querySelector(`cart-item-total`);
-            console.log('Quantity changed to:', quantitySelect.price);
-            cartItemTotal.textContent = `$${(Number(quantitySelect.value) * Number(itemPrice.textContent.replace('$', ''))).toFixed(2)}`;
+            const cartItemTotal=document.querySelector(`cart-item-total`);
+             const cartItemPrice=document.querySelector(`cart-item-total`);
+            
+            cartItemTotal.textContent = `$${(Number(quantitySelected) * Number(cartItemPrice.textContent.replace('$', ''))).toFixed(2)}`;
              updatePurchaseTotal();
         });
 };
