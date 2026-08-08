110bd3a0219efc26a8f5096d265169455cd50cc9
Dan Rollans <danrollans@yahoo.com>
2026-08-08 03:40:09 -0700

    Comment out redirection to cart.php in setupGlobalInteractions and improve console log in setupPurchasePage

--- js/danJS.js
+++ js/danJS.js
@@ -236,7 +236,7 @@ function setupGlobalInteractions() {
             }
             console.log("Proceeding to checkout with cart items:", cart);   
            
-            window.location.href = 'cart.php';
+            // window.location.href = 'cart.php';
             return;
         }
 
@@ -294,7 +294,7 @@ function setupPurchasePage() {
     initializeWishlistState();
     const orderCard = document.querySelector('.cart-main');
     const cart = getCart();
-console.log("Cart set page ", cart)
+    console.log("Cart set page ", cart);
     if (orderCard && cart.length) {
         const checkOutDetails = document.querySelector('.cart-items');
 
