c8fe5433211ca11018286c8419245d157f014c68
Dan Rollans <danrollans@yahoo.com>
2026-08-08 03:23:59 -0700

    Remove setupPurchasePage call on remove button click in cart

--- js/danJS.js
+++ js/danJS.js
@@ -245,7 +245,6 @@ function setupGlobalInteractions() {
             console.log('Remove button clicked:', removeButton);
             const index = Number(removeButton.dataset.itemIndex);
             removeCartItem(index);
-             setupPurchasePage();
             return;
         }
     });
