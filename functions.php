9e3ac09f54cbe04ae253f559c913d45bdf8c16ab
Dan Rollans <dan@spyninjashq.com>
2026-08-10 03:03:13 -0700

    Replace cart link with button for improved accessibility and interaction

--- functions.php
+++ functions.php
@@ -56,7 +56,7 @@
             <a class="cart-link links" href="#" aria-label="View cart"></a>
             <!-- Removed SEARCH menu item -->
             <!-- <a href="#"><span class="icon-account"></span>ACCOUNT</a> -->
-            <a href="cart.php" id="cart-button" class="cart-link" aria-label="View cart">
+            <button id="cart-button" class="cart-link" aria-label="View cart">
                 <svg id="cart-icon" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 100">
                     <path class="st1" class="cart-path" fill-rule="evenodd" clip-rule="evenodd"
                         d="M86.6,8v45.4H27.3L15.7,8h70.9M94.6,0H5.4l15.7,61.4h73.5V0h0Z" />
@@ -78,7 +78,7 @@
                 </svg>
                 <i id="cart-badge" class="menu-toggle cart-badge fa-regular fa-gem"></i>
                 <!-- <span class="cart-badge" aria-hidden="true">♦</span> -->
-            </a>
+</button>
             </div>
 
     </header>
