9bf0dedba1a272237684887601380c6d2201e928
Dan Rollans <dan@spyninjashq.com>
2026-08-15 03:11:14 -0700

    Declare makeCartChildren variable with proper scope in cart panel setup

--- js/danJS2.js
+++ js/danJS2.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const productQty = sideMenu.querySelector('.addItemsToOrder');
       const checkoutTotal = sideMenu.querySelector('.cart-item-total');
       const cartItems = sideMenu.querySelector('.cart-items');
-      const makeCartChildren;
+      let makeCartChildren;
   /* your logic */ 
       
 
