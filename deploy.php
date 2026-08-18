eade883885361a8fe5d46af0a49cff2c9eeca492
Dan Rollans <dan@spyninjashq.com>
2026-08-18 02:10:21 -0700

    Update deployment paths in deploy.php and modify hero section heading in index.php

--- deploy.php
+++ deploy.php
@@ -7,10 +7,10 @@ ob_end_clean();
 // 1. "Update from Remote" (Pull latest changes from GitHub)
 // The '/usr/local/cpanel/bin/' prefix ensures HostGator can find the cPanel tool.
 // The '2>&1' forces hidden errors to show up in your GitHub logs.
-$pull = shell_exec("/usr/local/cpanel/bin/uapi VersionControl update repository_root='/home2/sazxjwte/repositories/karensjewelrybox' 2>&1");
 
+/home2/sazxjwte/public_html/website_14f8c164
 // 2. "Deploy HEAD Commit" (Runs your .cpanel.yml instructions to copy files live)
-$deploy = shell_exec("/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root='/home2/sazxjwte/repositories/karensjewelrybox' 2>&1");
+$deploy = shell_exec("/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root='/home2/sazxjwte/public_html/website_14f8c164' 2>&1");
 
 // Print the logs so GitHub can capture the results
 echo "GitHub Webhook Triggered Successfully (No Secret Required)!\n\n";
